f292dfe8a05b01603e79a4453d0f9ce31ca093e6
Your Name <you@example.com>
2025-03-23 02:34:10 +0600

    slove 2nd problem

--- project-1/src/index.ts
+++ project-1/src/index.ts
@@ -1,5 +1,14 @@
+
+// Problem 1
 function sumArray(numbers: number[]): number {
     return numbers.reduce((sum, num) => sum + num, 0);
 }
 
-console.log(sumArray([1, 3, 5, 2, 5, 6]));
+
+// Problem 2
+
+function removeDuplicates(numbers: number[]): number[] {
+    return Array.from(new Set(numbers));
+}
+
+// console.log(removeDuplicates([1,3,3,4,4,5,5,6,6,6,7,8,8,9]));
