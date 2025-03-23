5bdd695fdd28596e2368f7830d931083b6b0c79e
Your Name <you@example.com>
2025-03-23 17:45:27 +0600

    slove problem 4

--- project-1/src/index.ts
+++ project-1/src/index.ts
@@ -14,7 +14,25 @@ function removeDuplicates(numbers: number[]): number[] {
 // Problem 3
 
 function countWordOccurrences(sentence: string, word: string): number {
-    
+
     const words = sentence.toLowerCase().split(" ");
     return words.filter(w => w === word.toLowerCase()).length;
+}
+
+
+// Problem 4
+
+type Circle = { shape: "circle", radius: number };
+type Rectangle = { shape: "rectangle", width: number, height: number }
+type Shape = Circle | Rectangle;
+
+function calculateShapeArea(s: Shape): number {
+    if(s.shape === "circle" ) {
+        return parseFloat((Math.PI * s.radius ** 2).toFixed(2));
+    }
+
+    else{
+        return s.width * s.height;
+    
+    }
 }
